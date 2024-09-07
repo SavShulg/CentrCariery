bf847869831fd207b7baab7b94f6e6b4ad277217
Home <zavalnyy_saveliy@mail.ru>
2024-09-07 13:48:11 +1000

    Complete

--- src/main/java/com/gridnine/testing/Main.java
+++ src/main/java/com/gridnine/testing/Main.java
@@ -1,7 +1,29 @@
 package com.gridnine.testing;
 
+import java.util.List;
+
 public class Main {
+
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        List<Flight> flights = FlightBuilder.createFlights();
+        Filter fil = new FilterFlights();
+
+        System.out.println("Сегменты с датой прибытия раньше даты отъезда:");
+        fil.beforeDepartureDate(flights);
+        System.out.println();
+
+        System.out.println("Сегменты с датой отправления, предшествующей текущему моменту времени:");
+        fil.departureCurrentTime(flights);
+        System.out.println();
+
+        System.out.println("Все полеты:");
+        fil.flights(flights);
+        System.out.println();
+
+        System.out.println("Участки с общим временем пребывания на земле более двух часов:");
+        fil.moreTwoHours(flights);
+        System.out.println();
     }
 }
