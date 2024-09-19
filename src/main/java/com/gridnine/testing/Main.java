0fc93816ada485acdf8bb4513932b1266f846b67
Home <zavalnyy_saveliy@mail.ru>
2024-09-20 00:51:45 +1000

    Complete!

--- src/main/java/com/gridnine/testing/Main.java
+++ src/main/java/com/gridnine/testing/Main.java
@@ -1,29 +1,32 @@
 package com.gridnine.testing;
 
+import com.gridnine.testing.filter.FilterBeforeNow;
+import com.gridnine.testing.filter.FlightFilter;
+import com.gridnine.testing.filter.FlightsArrivalBeforeDeparture;
+
+import com.gridnine.testing.filter.FilterLongTimeWaiting;
+import com.gridnine.testing.test.Flight;
+import com.gridnine.testing.test.FlightBuilder;
+
 import java.util.List;
 
 public class Main {
-
-
     public static void main(String[] args) {
-
         List<Flight> flights = FlightBuilder.createFlights();
-        Filter fil = new FilterFlights();
+        System.out.println("Все перелеты:");
+        flights.forEach(System.out::println);
 
-        System.out.println("Сегменты с датой прибытия раньше даты отъезда:");
-        fil.beforeDepartureDate(flights);
-        System.out.println();
+        FlightFilter beforeNow = new FilterBeforeNow();
+        FlightFilter arrivalBeforeDeparture = new FlightsArrivalBeforeDeparture();
+        FlightFilter longTimeWaiting = new FilterLongTimeWaiting();
 
-        System.out.println("Сегменты с датой отправления, предшествующей текущему моменту времени:");
-        fil.departureCurrentTime(flights);
-        System.out.println();
+        System.out.println("Вылет до текущего момента времени:");
+        beforeNow.filter(flights).forEach(System.out::println);
 
-        System.out.println("Все полеты:");
-        fil.flights(flights);
-        System.out.println();
+        System.out.println("Сегменты с датой прилёта раньше даты вылета:");
+        arrivalBeforeDeparture.filter(flights).forEach(System.out::println);
 
-        System.out.println("Участки с общим временем пребывания на земле более двух часов:");
-        fil.moreTwoHours(flights);
-        System.out.println();
+        System.out.println("Перелеты, где общее время, проведённое на земле, превышает два часа:");
+        longTimeWaiting.filter(flights).forEach(System.out::println);
     }
 }
--- src/main/java/com/gridnine/testing/test/FlightBuilder.java
+++ src/main/java/com/gridnine/testing/test/FlightBuilder.java
@@ -1,18 +1,14 @@
-package com.gridnine.testing;
+package com.gridnine.testing.test;
+
+
 
 import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Objects;
-import java.util.stream.Collectors;
 
-/**
- * Factory class to get sample list of flights.
- */
-class FlightBuilder {
-    static List<Flight> createFlights() {
+public class FlightBuilder {
+    public static List<Flight> createFlights() {
         LocalDateTime threeDaysFromNow = LocalDateTime.now().plusDays(3);
         return Arrays.asList(
                 //A normal flight with two hour duration
@@ -32,67 +28,15 @@ class FlightBuilder {
                         threeDaysFromNow.plusHours(3), threeDaysFromNow.plusHours(4),
                         threeDaysFromNow.plusHours(6), threeDaysFromNow.plusHours(7)));
     }
-
     private static Flight createFlight(final LocalDateTime... dates) {
         if ((dates.length % 2) != 0) {
             throw new IllegalArgumentException(
-                    "you must pass an even number of dates");
+                    "вы должны передать четное количество дат");
         }
         List<Segment> segments = new ArrayList<>(dates.length / 2);
         for (int i = 0; i < (dates.length - 1); i += 2) {
             segments.add(new Segment(dates[i], dates[i + 1]));
         }
         return new Flight(segments);
-    }
-}
-
-/**
- * Bean that represents a flight.
- */
-class Flight {
-    private final List<Segment> segments;
-
-    Flight(final List<Segment> segs) {
-        segments = segs;
-    }
-
-    List<Segment> getSegments() {
-        return segments;
-    }
-
-    @Override
-    public String toString() {
-        return segments.stream().map(Object::toString)
-                .collect(Collectors.joining(" "));
-    }
-}
-
-/**
- * Bean that represents a flight segment.
- */
-class Segment {
-    private final LocalDateTime departureDate;
-
-    private final LocalDateTime arrivalDate;
-
-    Segment(final LocalDateTime dep, final LocalDateTime arr) {
-        departureDate = Objects.requireNonNull(dep);
-        arrivalDate = Objects.requireNonNull(arr);
-    }
-
-    LocalDateTime getDepartureDate() {
-        return departureDate;
-    }
-
-    LocalDateTime getArrivalDate() {
-        return arrivalDate;
-    }
-
-    @Override
-    public String toString() {
-        DateTimeFormatter fmt =
-                DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm");
-        return '[' + departureDate.format(fmt) + '|' + arrivalDate.format(fmt)
-                + ']';
     }
 }
